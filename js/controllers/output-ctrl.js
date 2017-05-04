fed516991e002dd63850a4ee8808941d0734d4bf
AutumnCharnesky <autumn.breeze.c18@gmail.com>
2017-05-04 09:31:53 -0400

    story

--- js/controllers/output-ctrl.js
+++ js/controllers/output-ctrl.js
@@ -4,17 +4,17 @@ var app=angular.module('madMod');
 
     console.log('output ctrl');
 
-    var story1=["once upon a time there was a {{noun1}} they {{adj1}} and {{verb2}} and {{adj2}} and {{noun2}} and {{noun3}}", "yo {{noun1}} they {{adj1}} yo {{verb2}} and {{adj2}} and {{noun2}} and {{noun3}}"]
-
-
-
-
-    $scope.novel=[];
-    $scope.madlib1= function(){
-      var i=Math.floor(Math.random()*story1.length);
-      $scope.novel.push({text: story1[i] })
-        console.log($scope.novel);
-    }
+    // var story1=["once upon a time there was a"= $scope.noun1 +"they {{adj1}} and {{verb2}} and {{adj2}} and {{noun2}} and {{noun3}}", "yo {{noun1}} they {{adj1}} yo {{verb2}} and {{adj2}} and {{noun2}} and {{noun3}}"]
+    //
+    //
+    //
+    //
+    // $scope.novel=[];
+    // $scope.madlib1= function(){
+    //   var i=Math.floor(Math.random()*story1.length);
+    //   $scope.novel.push({text: story1[i] })
+    //     console.log($scope.novel);
+    // }
 
 
     $scope.print=madFactory.returnInputs();
